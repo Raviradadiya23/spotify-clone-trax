86cd5df67a9606bb14a531310617b17814b5bd90
Ravi Radadiya <raviradadiya011@gmail.com>
2022-09-14 12:23:33 +0530

    feat: congifure the playlist table and routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,56 @@
+import { Box, Text, Flex } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
+import { useMe } from "../lib/hooks";
 
-const Home = () => {
+const Home = ({ artists }) => {
+  const { user } = useMe();
   return (
     <GradientLayout
       roundImage
       color="gray"
       subtitle="profile"
-      title="Ravi Radadiya"
-      description="24 public playlists"
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user?.playlistsCount} public playlists`}
       image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
     >
-      <div>Home</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="cover.jpg"
+                  borderRadius="100%"
+                  height="250px"
+                  width="300px"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
