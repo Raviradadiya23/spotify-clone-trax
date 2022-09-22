f31b468f71483431ba8c438b423ed1bb9339f0a3
Ravi Radadiya <raviradadiya011@gmail.com>
2022-09-22 10:32:19 +0530

    fix the 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import Link from "next/link";
 
@@ -6,18 +7,18 @@ const PageNotFound = () => {
     <section className="page_404">
       <div className="container">
         <div className="row">
-          <div className="col-sm-12 parent">
-            <div className="col-sm-10 col-sm-offset-1  text-center">
+          <div className="col-sm-12 ">
+            <div className="col-sm-10 col-sm-offset-1  text-center parent">
               <div className="four_zero_four_bg">
                 <h1 className="text-center ">404</h1>
               </div>
 
-              <div className="contant_box_404">
+              <div className="contant_box_404 centerDiv">
                 <h3 className="h2">Look like you are lost</h3>
 
                 <p>the page you are looking for not avaible!</p>
               </div>
-              <div className="link_404">
+              <div className="link_404 centerDiv">
                 <Link href="/">
                   <a>Go To Home</a>
                 </Link>
